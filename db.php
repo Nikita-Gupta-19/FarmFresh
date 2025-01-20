80863c30db638fda97757b3045073d275df149bf
KHUSHI <rathorekr23@gmail.com>
2025-01-20 23:00:58 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+
+    $serverName = "localhost";
+    $userName = "root";
+    $password = "eshnasql";
+    $dbName = "agroculture";
+
+    $conn = mysqli_connect($serverName, $userName, $password, $dbName);
+    if (!$conn)
+    {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+
+
+?>
